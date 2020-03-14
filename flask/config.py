a500a27554141925f62650c497f9190395aa609c
zlaomin <zlaomamin@gmail.com>
2020-03-14 10:49:32 -0400

    init

---
+++ flask/config.py
@@ -0,0 +1,12 @@
+import os
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'oatest'
+MONGO_PASSWORD = 'testoa'
+MONGO_DBNAME = 'flask'
+MONGO_URI = "mongodb://localhost:27017/flask"
+
+TEST_DB = "test"
+TEST_URI = "mongodb://localhost:27017/test"
